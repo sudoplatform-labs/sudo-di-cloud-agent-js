52e3d6ccce711feb41953422b50110a82e1fd691
Sudo Platform Engineering <sudoplatform-engineering@anonyome.com>
2023-05-18 04:19:37 +0000

    Release 0.6.6

--- src/clientTools/versions.sh
+++ src/clientTools/versions.sh
@@ -1,10 +1,10 @@
-# This is a simple bash variable file to contain all the 
+# This is a simple bash variable file to contain all the
 # version and location information of third party components
 # which get built into the SDK
 
 # Aries Cloud Agent build config defaults
 ACA_PY_GIT_REPO_DEFAULT="https://github.com/anonyome/aries-cloudagent-python.git"
-ACA_PY_GIT_TAG_DEFAULT=0.700.400
+ACA_PY_GIT_TAG_DEFAULT=0.800.000
 ACA_PY_DOCKER_IMAGE_DEFAULT="sudo-di-cloud-agent"
 
 # Indy Tails File Server build config defaults
